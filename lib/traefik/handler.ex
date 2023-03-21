54fa54a950d2642a880e61eaf19a3febad3d7efb
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-21 15:54:28 -0600

    Pattern matching en funciones nombradas!

--- lib/traefik/handler.ex
+++ lib/traefik/handler.ex
@@ -17,7 +17,15 @@ defmodule Traefik.Handler do
   end
 
   def route(conn) do
+    route(conn, conn.method, conn.path)
+  end
+
+  def route(conn, "GET", "/developers") do
     %{conn | response: "Hello MakingDevs"}
+  end
+
+  def route(conn, "GET", "/projects") do
+    %{conn | response: "Traefik, Agora, Domino"}
   end
 
   def format_response(conn) do
@@ -33,6 +41,17 @@ end
 
 request = """
 GET /developers HTTP/1.1
+Host: makingdevs.com
+User-Agent: MyBrowser/0.1
+Accept: */*
+
+"""
+
+response = Traefik.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /projects HTTP/1.1
 Host: makingdevs.com
 User-Agent: MyBrowser/0.1
 Accept: */*
