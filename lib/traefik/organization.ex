e4b7cf23d2b2af9f1f0eca44929b520ea228fbb3
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-28 14:29:00 -0600

    Remove HEADER from string list

--- lib/traefik/organization.ex
+++ lib/traefik/organization.ex
@@ -2,10 +2,18 @@ defmodule Traefik.Organization do
   @csv_file Path.expand("../..", __DIR__)
 
   def list_developers() do
-    @csv_file
-    |> Path.join("MOCK_DATA.csv")
-    |> File.read!()
-    |> String.split("\n")
+    devs_in_string =
+      @csv_file
+      |> Path.join("MOCK_DATA.csv")
+      |> File.read!()
+      |> String.trim()
+      |> String.split("\n")
+      |> IO.inspect(label: "DEVS STRING")
+
+    [_, devs] = devs_in_string
+
+    devs
     |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(&Traefik.Developer.dev_from_list/1)
   end
 end
