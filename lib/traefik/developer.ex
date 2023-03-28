fd5aaabba15f496ee5def3b9cfe159ef183ec6ad
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-28 14:29:12 -0600

    Add fn dev_from_list

--- lib/traefik/developer.ex
+++ lib/traefik/developer.ex
@@ -1,3 +1,14 @@
 defmodule Traefik.Developer do
   defstruct id: 0, first_name: "", last_name: "", email: "", gender: "", ip_address: ""
+
+  def dev_from_list([id, first_name, last_name, email, gender, ip_address]) do
+    %__MODULE__{
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      gender: gender,
+      ip_address: ip_address
+    }
+  end
 end
