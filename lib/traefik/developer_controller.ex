765c763ab0c88ce9bf4a75285c09ae145a4054fc
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-28 14:52:51 -0600

    Apply Enum fn for a list of developers

--- lib/traefik/developer_controller.ex
+++ lib/traefik/developer_controller.ex
@@ -1,8 +1,13 @@
 defmodule Traefik.DeveloperController do
   def index(conn) do
-    developers = Traefik.Organization.list_developers()
-    response = "#{inspect(developers)}"
-    %{conn | status: 200, response: response}
+    response =
+      Traefik.Organization.list_developers()
+      |> Enum.filter(fn d -> d.gender != "Female" && d.gender != "Male" end)
+      |> Enum.sort(fn d1, d2 -> d1.last_name < d2.last_name end)
+      |> Enum.take(10)
+      |> Enum.map(fn d -> "<li>#{d.id} #{d.first_name} #{d.last_name} #{d.gender}<li>\n" end)
+
+    %{conn | status: 200, response: "<ul>#{response}</ul>"}
   end
 
   def show(conn, _params) do
