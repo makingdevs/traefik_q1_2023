ec0571b0bc29a0d179c9d256f553f346559798ea
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-29 15:30:25 -0600

    Use ExUnit test and DocTest

--- lib/traefik/parser.ex
+++ lib/traefik/parser.ex
@@ -20,8 +20,30 @@ defmodule Traefik.Parser do
     }
   end
 
+  @doc """
+  Given a params in string on the form 'key1=value1&key2=value2', transforms
+  those params into a map
+
+  ## Examples
+      iex> params_string = "foo=bar&zoo=boo&coo=hoo"
+      iex> Traefik.Parser.parse_params_string(params_string)
+      %{"foo" => "bar", "zoo" => "boo", "coo" => "hoo"}
+      iex> Traefik.Parser.parse_params_string("")
+      %{}
+  """
   def parse_params_string(params_string), do: String.trim(params_string) |> URI.decode_query()
 
+  @doc """
+  Parse a list of string headers into a map, in the form of ["Foo: Bar", "Date: 01/01/01"]
+  then transforms into a map with those keys and values
+
+  ## Example
+      iex> headers_string = ["Foo: Bar", "Date: 01/01/01"]
+      iex> Traefik.Parser.parse_headers(headers_string, %{})
+      %{"Foo" => "Bar", "Date" => "01/01/01"}
+      iex> Traefik.Parser.parse_headers([], %{})
+      %{}
+  """
   def parse_headers([], headers), do: headers
 
   def parse_headers([header_string | rest], headers) do
