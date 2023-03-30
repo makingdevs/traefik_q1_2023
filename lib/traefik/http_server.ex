3a0451c8584d62c140e868b54460d5a48a6348ea
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-30 15:59:00 -0600

    Add a server for traefik

---
+++ lib/traefik/http_server.ex
@@ -0,0 +1,58 @@
+defmodule Traefik.HttpServer do
+  def start(port) do
+    {:ok, listen_socket} =
+      :gen_tcp.listen(port, [:binary, packet: :raw, active: false, reuseaddr: true])
+
+    IO.puts("🎧 Listen on port #{port}...")
+    accept_loop(listen_socket)
+  end
+
+  def accept_loop(listen_socket) do
+    IO.puts("🙋 Waits for a client connection...")
+    {:ok, socket} = :gen_tcp.accept(listen_socket)
+    IO.puts("👫 Client connected ...")
+    serve(socket)
+    accept_loop(listen_socket)
+  end
+
+  def serve(client_socket) do
+    client_socket
+    |> read()
+    |> handle()
+    |> write_response(client_socket)
+  end
+
+  def read(client_socket) do
+    {:ok, request} = :gen_tcp.recv(client_socket, 0)
+    IO.puts("📧 Receive the request...")
+    IO.inspect(request, label: "REQUEST")
+    request
+  end
+
+  def handle(_request) do
+    """
+    HTTP/1.1 200 OK
+    Host: makingdevs.com
+    User-Agent: telnet
+    Content-Type: text/html
+    Accept: */*
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title></title>
+    </head>
+    <body>
+      <h1>HOLA MAKINGDEVS</h1>
+    </body>
+    </html>
+    """
+  end
+
+  def write_response(response, client_socket) do
+    :ok = :gen_tcp.send(client_socket, response)
+    IO.puts("    Response sent ...")
+    :ok = :gen_tcp.close(client_socket)
+  end
+end
