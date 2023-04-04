b2c435fcc7b4bc1e5f5293bd07225d9bf0fabd1b
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-04-04 15:38:48 -0600

    Add Client and Server API

--- lib/traefik/stack_server.ex
+++ lib/traefik/stack_server.ex
@@ -1,4 +1,19 @@
 defmodule Traefik.StackServer do
+  # Client API
+  def start do
+    IO.puts("🌤 Starts the Server")
+    spawn(__MODULE__, :loop, [[]])
+  end
+
+  def put(pid, element) do
+    send(pid, {self(), :put, element})
+  end
+
+  def get(pid) do
+    send(pid, {self(), :get})
+  end
+
+  # Server API
   def loop(state) do
     receive do
       {_caller, :put, elem} ->
