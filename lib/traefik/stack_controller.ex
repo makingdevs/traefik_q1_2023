04cf9675a7079e3329e275dbb86424788f58179e
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-04-04 15:54:44 -0600

    Use the Stack Server

--- lib/traefik/stack_controller.ex
+++ lib/traefik/stack_controller.ex
@@ -2,12 +2,14 @@ defmodule Traefik.StackController do
   alias Traefik.Conn
 
   def index(%Conn{} = conn, _params \\ %{}) do
-    result = []
+    result = Traefik.StackServer.get()
     %Conn{conn | status: 200, response: "#{inspect(result)}"}
   end
 
   def create(%Conn{} = conn, params) do
     IO.inspect("Put in stack: #{inspect(params)}")
+
+    Traefik.StackServer.put(params["value"])
     %Conn{conn | status: 203, response: "OK"}
   end
 end
