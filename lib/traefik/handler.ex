91b7666f2d3d7a67eab90410a203ea355f9184e2
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-04-04 14:25:10 -0600

    Set order of invocation result with pin oprator

--- lib/traefik/handler.ex
+++ lib/traefik/handler.ex
@@ -24,32 +24,29 @@ defmodule Traefik.Handler do
   def route(%Conn{method: "GET", path: "/fibonacci"} = conn) do
     IO.inspect(self(), label: "SELF")
     parent = self()
-    spawn(fn -> send(parent, {:ok, Traefik.Fibonacci.sequence(42)}) end)
-
-    spawn(fn -> send(parent, {:ok, Traefik.Fibonacci.sequence(43)}) end)
-
-    spawn(fn -> send(parent, {:ok, Traefik.Fibonacci.sequence(44)}) end)
-
-    spawn(fn -> send(parent, {:ok, Traefik.Factorial.of_time(50)}) end)
+    pid_1 = spawn(fn -> send(parent, {:ok, self(), Traefik.Fibonacci.sequence(44)}) end)
+    pid_2 = spawn(fn -> send(parent, {:ok, self(), Traefik.Fibonacci.sequence(43)}) end)
+    pid_3 = spawn(fn -> send(parent, {:ok, self(), Traefik.Fibonacci.sequence(42)}) end)
+    pid_4 = spawn(fn -> send(parent, {:ok, self(), Traefik.Factorial.of_time(50)}) end)
 
     r1 =
       receive do
-        {:ok, r} -> r
+        {:ok, ^pid_1, r} -> r
       end
 
     r2 =
       receive do
-        {:ok, r} -> r
+        {:ok, ^pid_2, r} -> r
       end
 
     r3 =
       receive do
-        {:ok, r} -> r
+        {:ok, ^pid_3, r} -> r
       end
 
     r_fac =
       receive do
-        {:ok, r} -> r
+        {:ok, ^pid_4, r} -> r
       end
 
     results = [r1, r2, r3]
